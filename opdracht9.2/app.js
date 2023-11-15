7219f083ec28004d9c215723263d3950a0c0dcb4
9021948 <144111943+9021948@users.noreply.github.com>
2023-11-15 10:42:36 +0100

    9.2

---
+++ opdracht9.2/app.js
@@ -0,0 +1,14 @@
+.center {
+    display: flex;
+    width: 100%;
+    height: 90vh;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column
+}
+.monkey {
+    font-size:1000%;
+}
+#closed {
+    display: None; 
+}
