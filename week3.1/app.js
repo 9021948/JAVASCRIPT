7c66e3eb6da89acb01b0cd3a659f926d96e984d7
9021948 <144111943+9021948@users.noreply.github.com>
2023-12-07 18:35:04 +0100

    kl

---
+++ week3.1/app.js
@@ -0,0 +1,10 @@
+const tech = ["playStation", "xbox", "pc"];
+
+let text = "";
+for (let i = 0; i < tech.length;  i++) {
+  text += tech[i] + "<br>";
+}
+
+document.getElementById("j").innerHTML = "Er zitten " + tech.length + "producten in uw winkelmand.";
+
+document.getElementById("a").innerHTML = "Uw producten zijn: <br>" + text;
