4bdde8680c6c555ddc2d3a7dc35f2865932e7165
9021948 <144111943+9021948@users.noreply.github.com>
2023-12-13 11:19:10 +0100

    klaar

--- Game/java.js
+++ Game/java.js
@@ -8,15 +8,65 @@ document.body.appendChild(canvas);
 // Variabelen voor de positie en snelheid van de cirkel
 let x = 50; // startpositie X
 let y = 50; // startpositie Y
-let dx = 6; // snelheid X    van bal
+let dx = 6; // snelheid X    
 let dy = 4; // snelheid Y
 let radius = 20; // radius van de cirkel
+
+// variabelen voor de 2e bal
+let x2 = 100;
+let y2 = 150;
+let speed = 4;
+let radius2 = 10;
+
+document.addEventListener('keydown', function(event){
+    if (Event.key === 'ArrowLeft') {
+        x2 -= speed;
+        if  (x2 < radius2) {
+            x2 = canvas.width - radius2;
+            x2 = radius2;
+        }
+
+    } 
+    if (event.key === 'ArrowUp')
+    else if (event.key === 'ArrowRight'){
+         x2 += speed; 
+     if (x2 > canvas.witdh - radius2){
+         x2 = radius2;
+     }
+    }
+});
+if (event.key == 'arrowUp && y2 > radius2'){
+    y2 -= speed;
+
+}
+function collission() {
+     if (x == x2 && y == y2) {
+        alert (Game Over);
+     }
+}
+
+// starten van bewegen bal 1
+function startBewegen() {
+    dx = 2;
+    dy = 2;
+}
+// functie om te klik op button om te zetten in uivoeren functie startbewegen
+document.getElementById('startButton').addEventListener('click', startBewegen);
  
-// De functie om de cirkel te tekenen
+// De functie om de cirkel te tekenen bal 1
 function drawCircle() {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2, true);
     ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// De functie om de cirkel te tekenen bal2
+function drawCircle2() {
+    ctx.beginPath();
+    ctx.arc(x2, y2, radius2, 0, Math.PI * 2, true);
+    ctx.fillStyle = 'green';
     ctx.fill();
     ctx.closePath();
 }
@@ -28,6 +78,9 @@ function update() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
  
     drawCircle();
+    drawCircle2();
+    collission
+
  
     // Verander de positie van de cirkel
     x += dx;
